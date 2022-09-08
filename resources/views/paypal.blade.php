6ec5e88b85d9cff3fd29c46d48102ea09cd77235
kashmala ali <kashmalaali31@gmail.com>
2022-09-08 16:04:36 +0500

    changes

--- resources/views/paypal.blade.php
+++ resources/views/paypal.blade.php
@@ -19,7 +19,7 @@
     <style>
         .bg-santa-img {
             background-image: url(<?php echo request()->query("a"); ?>);
-            width: 75%;
+            width: 100%;
             border: 3px solid gray;
             height: 365px;
             margin-top: 20px !important;
@@ -68,7 +68,7 @@
                      Pay<span class="sky-clr">Pal</span></a>
                     </div>
                     <div class="paypal text-white venmo">
-                     <a id="venmo-button" href="">venmo</a>
+                     <a id="venmo-button" href="">Venmo</a>
                     </div>
                     <div class="paypal text-white credit">
                         <!-- <i class="fa-brands fa-paypal"></i> -->
