7db1c7ab4006d123d9f843bffd10371bfb84afeb
kashmala ali <kashmalaali31@gmail.com>
2022-11-07 20:25:01 +0500

    changes

--- resources/views/image-test.blade.php
+++ resources/views/image-test.blade.php
@@ -18,6 +18,9 @@
     <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
 
     <link rel="stylesheet" href="https://code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+    
+    <script src="https://cdn.jsdelivr.net/npm/interactjs/dist/interact.min.js"></script>
+    <script src="https://unpkg.com/interactjs/dist/interact.min.js"></script>
     <style>
         @media only screen and (min-width: 320px) {
             .bg-santa-img {
@@ -270,6 +273,7 @@
             <div class="bg-santa-img showimg w-100" id="my-node">
                 <p class="rotingtxt1">{{$watermark}}</p>
                 <div id="container">
+                    <div id="gesture-area">
                     <div id="canvas-border-hide">
                         <div class="right"></div>
                         <div class="bottom"></div>
@@ -279,17 +283,10 @@
                         <div data-clickable="true" class="topLeft"></div>
                         <div data-clickable="true" class="topRight"></div>
                         <div data-clickable="true" class="bottomLeft"></div>
-                    </div>
-                    
-                    <!-- <div class="page pinch-zoom-parent"> -->
-                        <!-- <div class="pinch-zoom">
-
-                            <img src={{asset('assets/imgs/santa12.png')}} id="santaImg" class=" image santa-dp4 border-class"
-                            alt="missing">
-                        </div> -->
-                        <div class="pinch-zoom-container">
-                            <img class=" pinch-zoom-image image santa-dp4 border-class"  id="pinch-zoom-image-id" onload="onLoad()" src={{asset('assets/imgs/santa12.png')}} alt="missing"/>
+                        
+                            <img src={{asset('assets/imgs/santa12.png')}} class='santaImg image santa-dp4 border-class' alt="sample image" id="scale-element">
                         </div>
+                    </div>
                     <!-- </div> -->
                 </div>
             </div>
@@ -320,7 +317,6 @@
 </div>
 
 
-</script>
 <script>
 
     function disableScroll() {
@@ -394,39 +390,7 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.20.3/TweenMax.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.20.3/utils/Draggable.min.js"></script>
 <!--<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>-->
-<script>
-//     $(document).ready(function(){
-//         var fixmeTop = $('.fixme').offset().top;       // get initial position of the element
-//         $(window).scroll(function() {                  // assign scroll event listener
 
-//         var currentScroll = $(window).scrollTop(); // get current position
-//         if (currentScroll >= fixmeTop) {           // apply position: fixed if you
-//             $('.fixme').css({                      // scroll to that element or below it
-//                 position: 'fixed',
-//                 top: '0',
-//                 left: '0'
-//             });
-//         } else {                                   // apply position: static
-//             $('.fixme').css({                      // if you scroll above it
-//                 position: 'static'
-//             });
-//         }   
-//     });
-// })
-$(function(){
-        var stickyHeaderTop = $('#stickytypeheader').offset().top;
- 
-        $(window).scroll(function(){
-                if( $(window).scrollTop() > stickyHeaderTop ) {
-                        $('#stickytypeheader').css({position: 'sticky', top: '0px' ,  width: '100%' });
-                        
-                } else {
-                        $('#stickytypeheader').css({position: 'static', top: '0' ,  width: '100%' });
-                      
-                }
-        });
-  });
-  </script>
 <script>
     $(document).ready(function () {
         init();
@@ -641,208 +605,70 @@ $(function(){
 
 </script>
 <script>
-        
-        var MIN_SCALE = 1; // 1=scaling when first loaded
-        var MAX_SCALE = 64;
-        var imgWidth = null;
-        var imgHeight = null;
-        var viewportWidth = null;
-        var viewportHeight = null;
-        var scale = null;
-        var lastScale = null;
-        var container = null;
-        var img = null;
-        var x = 0;
-        var lastX = 0;
-        var y = 0;
-        var lastY = 0;
-        var pinchCenter = null;
-  
-        // We need to disable the following event handlers so that the browser doesn't try to
-        // automatically handle our image drag gestures.
-        var disableImgEventHandlers = function () {
-          var events = ['onclick', 'onmousedown', 'onmousemove', 'onmouseout', 'onmouseover',
-                        'onmouseup', 'ondblclick', 'onfocus', 'onblur'];
-  
-          events.forEach(function (event) {
-            img[event] = function () {
-              return false;
-            };
-          });
-        };
-  
-        // Traverse the DOM to calculate the absolute position of an element
-        var absolutePosition = function (el) {
-          var x = 0,
-            y = 0;
-  
-          while (el !== null) {
-            x += el.offsetLeft;
-            y += el.offsetTop;
-            el = el.offsetParent;
-          }
-  
-          return { x: x, y: y };
-        };
-  
-        var restrictScale = function (scale) {
-          if (scale < MIN_SCALE) {
-            scale = MIN_SCALE;
-          } else if (scale > MAX_SCALE) {
-            scale = MAX_SCALE;
-          }
-          return scale;
-        };
-  
-        var restrictRawPos = function (pos, viewportDim, imgDim) {
-          if (pos < viewportDim/scale - imgDim) { // too far left/up?
-            pos = viewportDim/scale - imgDim;
-          } else if (pos > 0) { // too far right/down?
-            pos = 0;
-          }
-          return pos;
-        };
-  
-        var updateLastPos = function (deltaX, deltaY) {
-          lastX = x;
-          lastY = y;
-        };
-  
-        var translate = function (deltaX, deltaY) {
-          // We restrict to the min of the viewport width/height or current width/height as the
-          // current width/height may be smaller than the viewport width/height
-  
-          var newX = restrictRawPos(lastX + deltaX/scale,
-                                    Math.min(viewportWidth, curWidth), imgWidth);
-          x = newX;
-          img.style.marginLeft = Math.ceil(newX*scale) + 'px';
-  
-          var newY = restrictRawPos(lastY + deltaY/scale,
-                                    Math.min(viewportHeight, curHeight), imgHeight);
-          y = newY;
-          img.style.marginTop = Math.ceil(newY*scale) + 'px';
-        };
-  
-        var zoom = function (scaleBy) {
-          scale = restrictScale(lastScale*scaleBy);
-  
-          curWidth = imgWidth*scale;
-          curHeight = imgHeight*scale;
-  
-          img.style.width = Math.ceil(curWidth) + 'px';
-          img.style.height = Math.ceil(curHeight) + 'px';
-  
-          // Adjust margins to make sure that we aren't out of bounds
-          translate(0, 0);
-        };
-  
-        var rawCenter = function (e) {
-          var pos = absolutePosition(container);
-  
-          // We need to account for the scroll position
-          var scrollLeft = window.pageXOffset ? window.pageXOffset : document.body.scrollLeft;
-          var scrollTop = window.pageYOffset ? window.pageYOffset : document.body.scrollTop;
-  
-          var zoomX = -x + (e.center.x - pos.x + scrollLeft)/scale;
-          var zoomY = -y + (e.center.y - pos.y + scrollTop)/scale;
-  
-          return { x: zoomX, y: zoomY };
-        };
-  
-        var updateLastScale = function () {
-          lastScale = scale;
-        };
-  
-        var zoomAround = function (scaleBy, rawZoomX, rawZoomY, doNotUpdateLast) {
-          // Zoom
-          zoom(scaleBy);
-  
-          // New raw center of viewport
-          var rawCenterX = -x + Math.min(viewportWidth, curWidth)/2/scale;
-          var rawCenterY = -y + Math.min(viewportHeight, curHeight)/2/scale;
-  
-          // Delta
-          var deltaX = (rawCenterX - rawZoomX)*scale;
-          var deltaY = (rawCenterY - rawZoomY)*scale;
-  
-          // Translate back to zoom center
-          translate(deltaX, deltaY);
-  
-          if (!doNotUpdateLast) {
-            updateLastScale();
-            updateLastPos();
-          }
-        };
-  
-        var zoomCenter = function (scaleBy) {
-          // Center of viewport
-          var zoomX = -x + Math.min(viewportWidth, curWidth)/2/scale;
-          var zoomY = -y + Math.min(viewportHeight, curHeight)/2/scale;
-  
-          zoomAround(scaleBy, zoomX, zoomY);
-        };
-        var onLoad = function () {
-  
-          img = document.getElementById('pinch-zoom-image-id');
-          container = img.parentElement;
-  
-          disableImgEventHandlers();
-  
-          imgWidth = img.width;
-          imgHeight = img.height;
-          viewportWidth = img.offsetWidth;
-          scale = viewportWidth/imgWidth;
-          lastScale = scale;
-          viewportHeight = img.parentElement.offsetHeight;
-          curWidth = imgWidth*scale;
-          curHeight = imgHeight*scale;
-  
-          var hammer = new Hammer(container, {
-            domEvents: true
-          });
-  
-          hammer.get('pinch').set({
-            enable: true
-          });
-  
-          hammer.on('pan', function (e) {
-            translate(e.deltaX, e.deltaY);
-          });
-  
-          hammer.on('panend', function (e) {
-            updateLastPos();
-          });
-  
-          hammer.on('pinch', function (e) {
-  
-            // We only calculate the pinch center on the first pinch event as we want the center to
-            // stay consistent during the entire pinch
-            if (pinchCenter === null) {
-              pinchCenter = rawCenter(e);
-              var offsetX = pinchCenter.x*scale - (-x*scale + Math.min(viewportWidth, curWidth)/2);
-              var offsetY = pinchCenter.y*scale - (-y*scale + Math.min(viewportHeight, curHeight)/2);
-              pinchCenterOffset = { x: offsetX, y: offsetY };
-            }
-            var newScale = restrictScale(scale*e.scale);
-            var zoomX = pinchCenter.x*newScale - pinchCenterOffset.x;
-            var zoomY = pinchCenter.y*newScale - pinchCenterOffset.y;
-            var zoomCenter = { x: zoomX/newScale, y: zoomY/newScale };
-  
-            zoomAround(e.scale, zoomCenter.x, zoomCenter.y, true);
-          });
-  
-          hammer.on('pinchend', function (e) {
-            updateLastScale();
-            updateLastPos();
-            pinchCenter = null;
-          });
-  
-          hammer.on('doubletap', function (e) {
-            var c = rawCenter(e);
-            zoomAround(2, c.x, c.y);
-          });
-  
-        };
-        </script>
+        function dragMoveListener (event) {
+    var target = event.target,
+        // keep the dragged position in the data-x/data-y attributes
+        x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+        y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+    // translate the element
+    target.style.webkitTransform =
+    target.style.transform =
+      'translate(' + x + 'px, ' + y + 'px)';
+
+    // update the posiion attributes
+    target.setAttribute('data-x', x);
+    target.setAttribute('data-y', y);
+  }
+//   ----
+        var angleScale = {
+  angle: 0,
+  scale: 1
+}
+var gestureArea = document.getElementById('gesture-area')
+var scaleElement = document.getElementById('scale-element')
+var resetTimeout
+
+interact(gestureArea)
+  .gesturable({
+    listeners: {
+      start (event) {
+        angleScale.angle -= event.angle
+
+        clearTimeout(resetTimeout)
+        scaleElement.classList.remove('reset')
+      },
+      move (event) {
+        // document.body.appendChild(new Text(event.scale))
+        var currentAngle = event.angle + angleScale.angle
+        var currentScale = event.scale * angleScale.scale
+
+        scaleElement.style.transform =
+          'rotate(' + currentAngle + 'deg)' + 'scale(' + currentScale + ')'
+
+        // uses the dragMoveListener from the draggable demo above
+        dragMoveListener(event)
+      },
+      end (event) {
+        angleScale.angle = angleScale.angle + event.angle
+        angleScale.scale = angleScale.scale * event.scale
+
+        // resetTimeout = setTimeout(reset, 1000)
+        // scaleElement.classList.add('reset')
+      }
+    }
+  })
+  .draggable({
+    listeners: { move: dragMoveListener }
+  })
+
+function reset () {
+  scaleElement.style.transform = 'scale(1)'
+
+  angleScale.angle = 0
+  angleScale.scale = 1
+}
+
+ </script>
 </body>
 </html>
