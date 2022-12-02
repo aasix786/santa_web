66a77ecc45eea50cd966fcf8aa237d4378f2678c
siddiquekhanonism <112387011+siddiquekhanonism@users.noreply.github.com>
2022-12-02 21:06:06 +0500

    new changes
    new chages

--- resources/views/paypal.blade.php
+++ resources/views/paypal.blade.php
@@ -29,14 +29,14 @@
 
    </script>
     <style>
-       /* body {
+    body {
             -webkit-touch-callout: none;
             -webkit-user-select: none;
             -khtml-user-select: none;
             -moz-user-select: none;
             -ms-user-select: none;
             user-select: none;
-        }*/
+        }
 
         @media only screen and (min-width: 320px) {
             .bg-santa-img {
@@ -301,7 +301,7 @@
                                                             <input autocomplete='off' class='form-control card-cvc' placeholder='ex. 311' size='4' type='text'>
                                                         </div>
                                                         <div class='col-xs-12 col-md-4 form-group expiration required'>
-                                                            <label class='control-label'>Expiration Month</label> 
+                                                            <label class='control-label'>Expiration Month</label>
                                                             <input class='form-control card-expiry-month' placeholder='MM' size='2' type='text'>
                                                         </div>
                                                         <div class='col-xs-12 col-md-4 form-group expiration required'>
@@ -332,7 +332,7 @@
                         </div>
                     </div>
                     </div>
-                <!-- Card payment End -->               
+                <!-- Card payment End -->
 
                 <div class="payment-method mt-0">
                     <div class="payment-cards"><img src="{{asset('assets/imgs/card1.png')}}" class="img-size"></div>
