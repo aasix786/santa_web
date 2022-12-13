e05b6f4a16522bc5979d4ae6116d1104220ce782
kashmala ali <kashmalaali31@gmail.com>
2022-12-13 22:29:10 +0500

    mala

--- resources/views/paypal.blade.php
+++ resources/views/paypal.blade.php
@@ -25,10 +25,18 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
+
    <script>
 
    </script>
     <style>
+        footer p{
+           
+    font-family: 'Bebas Neue', cursive; 
+        }
     body {
             -webkit-touch-callout: none;
             -webkit-user-select: none;
